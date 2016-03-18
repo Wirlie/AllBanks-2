6ac56901e68a4050d0a2e2d1c6a4071809403b6e
Wirlie <josue.211996@outlook.com>
2016-03-18 14:12:07 -0600

    Javadoc.

--- AllBanks/src/main/java/me/wirlie/allbanks/data/BankSession.java
+++ AllBanks/src/main/java/me/wirlie/allbanks/data/BankSession.java
@@ -33,6 +33,10 @@ import me.wirlie.allbanks.StringsID;
 import me.wirlie.allbanks.Translation;
 
 /**
+ * Esta clase se encarga de almacenar las sesiones de AllBanks cuando un jugador usa un banco (letrero),
+ * de esta manera esta clase siempre estará accesible desde sus métodos estáticos para acceder a los valores
+ * almacenados en el {@link BankSession#activeSessions HashMap activeSessions}
+ * 
  * @author Wirlie
  * @since AllBanks v1.0
  *
@@ -43,12 +47,24 @@ public class BankSession {
 	 * Funciones para las sesiones activas. (activeSessions) 
 	 */
 	
+	/**
+	 * Este HashMap contiene las sesiones activas en una especie de "caché".
+	 */
 	private static HashMap<UUID, BankSession> activeSessions = new HashMap<UUID, BankSession>();
 	
+	/**
+	 * Obtener todas las sesiones activas.
+	 * @return Colección de todas las sesiones.
+	 */
 	public static Collection<BankSession> getAllActiveSessions(){
 		return activeSessions.values();
 	}
 	
+	/**
+	 * Recuperar una sesión por medio de la instancia Sign (letrero).
+	 * @param sign - Instancia Sign (letrero).
+	 * @return Devuelve la sesión que pertenece a este letrero.
+	 */
 	public static BankSession getActiveSessionBySign(Sign sign){
 		
 		for(BankSession bs : activeSessions.values()){
@@ -60,6 +76,12 @@ public class BankSession {
 		return null;
 	}
 	
+	/**
+	 * Iniciar una nueva sesión ({@link me.wirlie.allbanks.data.BankSession#startSession(UUID, BankSession) Método similar})".
+	 * @param p - Jugador al que pertenece la sesión (Si ya existe una sessión con este jugador la anterior sessión será reemplazada).
+	 * @param bs - Datos de la sesión {@link me.wirlie.allbanks.data.BankSession BankSession}
+	 * @return Para fines prácticos se devuelve la variable <b>bs</b> sin modificar.
+	 */
 	public static BankSession startSession(Player p, BankSession bs){
 		startSession(p.getUniqueId(), bs);
 		
@@ -67,6 +89,12 @@ public class BankSession {
 		return bs;
 	}
 	
+	/**
+	 * Iniciar una nueva sesión.
+	 * @param uuid - UUID del jugador al que pertenece la sesión (Si ya existe una sesión con este jugador la anterior sesión será reemplazada).
+	 * @param bs - Datos de la sesión {@link me.wirlie.allbanks.data.BankSession BankSession}
+	 * @return Para fines prácticos se devuelve la variable <b>bs</b> sin modificar.
+	 */
 	public static BankSession startSession(UUID uuid, BankSession bs){
 		activeSessions.put(uuid, bs);
 		
@@ -74,18 +102,36 @@ public class BankSession {
 		return bs;
 	}
 	
+	/**
+	 * Obtener la sesión si es que existe ({@link me.wirlie.allbanks.data.BankSession#getSession(UUID) Método similar}).
+	 * @param p - Jugador al que pertenece la sesión.
+	 * @return Devuelve la instancia de la sesión almacenada ({@link BankSession}) o {@code null} si no existe una sesión para este jugador.
+	 */
 	public static BankSession getSession(Player p){
 		return getSession(p.getUniqueId());
 	}
 	
+	/**
+	 * Obtener la sesión si es que existe.
+	 * @param uuid - Jugador al que pertenece la sesión (UUID).
+	 * @return Devuelve la instancia de la sesión almacenada ({@link BankSession}) o {@code null} si no existe una sesión para este jugador.
+	 */
 	public static BankSession getSession(UUID uuid){
 		return activeSessions.get(uuid);
 	}
 	
+	/**
+	 * Cerrar una sesión.
+	 * @param p - Jugador al que pertenece la sesión.
+	 */
 	public static void closeSession(Player p){
 		closeSession(p.getUniqueId());
 	}
 	
+	/**
+	 * Cerrar una sesión.
+	 * @param uuid - Jugador al que pertenece la sesión (UUID).
+	 */
 	public static void closeSession(UUID uuid){
 		//notificar
 		Translation.getAndSendMessage(Bukkit.getPlayer(uuid), StringsID.SESSION_CLOSED, true);
@@ -97,18 +143,44 @@ public class BankSession {
 		activeSessions.remove(uuid);
 	}
 
+	/**
+	 * <ul>
+	 * Checar si una sesión ya existe, esto es útil para procesar métodos como {@link BankSession#getSession(UUID)} o {@link BankSession#closeSession(UUID)}.
+	 * Sin embargo no es necesario, ya que estos métodos pueden trabajar a pesar de que una sesión no exista.
+	 * </ul>
+	 * @param p - Jugador al que pertenece la sesión.
+	 * @return {@code true} si la sesión existe, o {@code false} si no.
+	 */
 	public static boolean checkSession(Player p){
 		return checkSession(p.getUniqueId());
 	}
 	
+	/**
+	 * <ul>
+	 * Checar si una sesión ya existe, esto es útil para procesar métodos como {@link BankSession#getSession(UUID)} o {@link BankSession#closeSession(UUID)}.
+	 * Sin embargo no es necesario, ya que estos métodos pueden trabajar a pesar de que una sesión no exista.
+	 * </ul>
+	 * @param uuid - Jugador al que pertenece la sesión (UUID).
+	 * @return {@code true} si la sesión existe, o {@code false} si no.
+	 */
 	public static boolean checkSession(UUID uuid){
 		return activeSessions.containsKey(uuid);
 	}
 	
+	/**
+	 * Actualizar una sesión con una instancia nueva de {@link BankSession}
+	 * @param p - Jugador al que se desea actualizar la sesión.
+	 * @param bs - Nueva instancia de {@link BankSession} (se reemplazará la anterior).
+	 */
 	public static void updateSession(Player p, BankSession bs){
 		startSession(p, bs);
 	}
 	
+	/**
+	 * Actualizar una sesión con una instancia nueva de {@link BankSession}
+	 * @param uuid - Jugador al que se desea actualizar la sesión (UUID).
+	 * @param bs - Nueva instancia de {@link BankSession} (se reemplazará la anterior).
+	 */
 	public static void updateSession(UUID uuid, BankSession bs){
 		startSession(uuid, bs);
 	}
@@ -117,11 +189,25 @@ public class BankSession {
 	 * Datos usados para BankSession.
 	 */
 	
+	/** Jugador, dueño de esta sesión. */
 	Player player;
+	
+	/** variable del tipo {@code Enum} que conserva el tipo del banco de esta sesión. */
 	BankType btype;
+	
+	/** Paso (step) actual del banco de esta sesión. */
 	int step;
+	
+	/** Letrero con el cual se está trabajando en esta sesión. */
 	Sign sign;
 	
+	/**
+	 * Inicializar una nueva instancia de {@link BankSession}
+	 * @param player - Jugador.
+	 * @param sign - Letrero con el que se va a trabajar.
+	 * @param btype - Tipo de banco al que pertenece el letrero con el que se va a trabajar.
+	 * @param step - Paso actual del letrero (banco).
+	 */
 	public BankSession(Player player, Sign sign, BankType btype, int step){
 		this.btype = btype;
 		this.step = step;
@@ -129,43 +215,75 @@ public class BankSession {
 		this.player = player;
 	}
 	
+	/**
+	 * @return Devuelve el tipo de banco de esta sesión ({@link BankType}).
+	 */
 	public BankType getBankType(){
 		return btype;
 	}
 	
+	/**
+	 * @return Devuelve el paso actual de esta sesión.
+	 */
 	public int getStep(){
 		return step;
 	}
 	
+	/**
+	 * @return Devuelve el letrero con el que se está trabajando ({@link Sign})
+	 */
 	public Sign getSign(){
 		return sign;
 	}
 	
+	/**
+	 * @return Devuelve el jugador al que pertenece esta sesión.
+	 */
 	public Player getPlayer(){
 		return player;
 	}
 	
+	/**
+	 * Actualizar paso y cambiar el letrero a este nuevo paso.
+	 * @param step - Paso a actualizar.
+	 * @param ignoreUpdate - Ignorar actualización de sesión.
+	 */
 	public void updateStepAndSwitchSign(int step, boolean ignoreUpdate){
+		this.step = step;
+		
 		if(!ignoreUpdate){
-			this.step = step;
 			updateSession(player.getUniqueId(), this);
 		}
 		
 		Banks.switchSignToStep(btype, sign, step);
 	}
 	
+	/**
+	 * Actualizar paso y cambiar el letrero a este nuevo paso.
+	 * @param step - Paso a actualizar.
+	 */
 	public void updateStepAndSwitchSign(int step){
 		updateStepAndSwitchSign(step, false);
 	}
 	
+	/**
+	 * Actualizar paso al siguiente, ejemplo, si el paso actual está definido en 1 con este método
+	 * se buscará avanzar al paso 2, y si está en el paso 2 se buscará avanzar al paso 3.
+	 */
 	public void updateStepAndSwitchSign(){
 		updateStepAndSwitchSign(Banks.getNextStep(this), false);
 	}
 	
+	/**
+	 * Cerrar sesión de esta instancia.
+	 */
 	public void closeSession(){
 		closeSession(player);
 	}
 	
+	/**
+	 * Actualizar sesión al paso inicial.
+	 */
 	private void updateToInitialState() {
 		updateStepAndSwitchSign(-1, true);
 	}
