01b79ca1101c80a9ef590ef2b80d9d4944f331de
Wirlie <josue.211996@outlook.com>
2016-03-23 00:11:37 -0600

    Add util for XP Conversion (MC 1.8).

--- AllBanks/src/main/java/me/wirlie/allbanks/Util.java
+++ AllBanks/src/main/java/me/wirlie/allbanks/Util.java
@@ -24,6 +24,7 @@ import java.util.regex.Pattern;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 /**
  * @author Wirlie
@@ -31,6 +32,122 @@ import org.bukkit.Location;
  *
  */
 public class Util {
+	
+	public static class XPConversionUtil{
+	    
+		/**
+		 * Establecer experiencia total al jugador.
+		 * @param player
+		 * @param totalExp
+		 */
+	    public static void setTotalExpToPlayer(Player player, int totalExp) {
+	        if (totalExp < 0) {
+	            throw new IllegalArgumentException("Experience can not have a negative value!");
+	        }
+	        
+	        //Reset exp and level...
+	        player.setExp(0);
+	        player.setLevel(0);
+	        player.setTotalExperience(0);
+
+	        //Recalculate experience
+	        int amount = totalExp;
+	        while (amount > 0) {
+	            final int expToLevel = getExpAtLevel(player);
+	            amount -= expToLevel;
+	            if (amount >= 0) {
+	                player.giveExp(expToLevel);
+	            } else {
+	                amount += expToLevel;
+	                player.giveExp(amount);
+	                amount = 0;
+	            }
+	        }
+	    }
+
+	    /**
+	     * Obtiene la cantidad de experiencia que se necesita para el sig nivel.
+	     * @param player
+	     * @return
+	     */
+	    private static int getExpAtLevel(final Player player) {
+	        return getExpAtLevel(player.getLevel());
+	    }
+
+	    /**
+	     * Obtiene la cantidad de experiencia que se necesita para el sig nivel.
+	     * @param level
+	     * @return
+	     */
+	    public static int getExpAtLevel(final int level) {
+	        if (level <= 15) {
+	            return (2 * level) + 7;
+	        }
+	        if ((level >= 16) && (level <= 30)) {
+	            return (5 * level) - 38;
+	        }
+	        return (9 * level) - 158;
+	    }
+	    
+	    public static int convertExpToLevel(final int xp){
+	        if (xp <= getExpAtLevel(15)) {
+	        	return (xp - 7) / 2;
+	        }else if ((xp >= getExpAtLevel(16)) && (xp <= getExpAtLevel(30))) {
+	        	return (xp + 38) / 5;
+	        }else{
+	        	return (xp + 158) / 9;
+	        }
+	    }
+	    
+	    /**
+	     * Obtiene la cantidad de experiencia total para el nivel especificado.
+	     * @param level
+	     * @return
+	     */
+	    public static int getExpToLevel(final int level) {
+	        int currentLevel = 0;
+	        int exp = 0;
+
+	        while (currentLevel < level) {
+	            exp += getExpAtLevel(currentLevel);
+	            currentLevel++;
+	        }
+	        if (exp < 0) {
+	            exp = Integer.MAX_VALUE;
+	        }
+	        return exp;
+	    }
+
+	    /**
+	     * Obtiene la cantidad total de experiencia quer tiene el jugador.
+	     * @param player
+	     * @return
+	     */
+	    public static int getTotalExperience(final Player player) {
+	        int exp = (int) Math.round(getExpAtLevel(player) * player.getExp());
+	        int currentLevel = player.getLevel();
+
+	        while (currentLevel > 0) {
+	            currentLevel--;
+	            exp += getExpAtLevel(currentLevel);
+	        }
+	        if (exp < 0) {
+	            exp = Integer.MAX_VALUE;
+	        }
+	        return exp;
+	    }
+
+	    /**
+	     * Obtener la experiencia necesaria para el siguiente nivel del jugador.
+	     * @param player
+	     * @return
+	     */
+	    public static int getExpUntilNextLevel(final Player player) {
+	        int exp = (int) Math.round(getExpAtLevel(player) * player.getExp());
+	        int nextLevel = player.getLevel();
+	        return getExpAtLevel(nextLevel) - exp;
+	    }
+	}
 
 	public static class ConfigUtil{
 		/**
