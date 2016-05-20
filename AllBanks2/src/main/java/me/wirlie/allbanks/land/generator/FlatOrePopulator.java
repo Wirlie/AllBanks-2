b04f814e89712e65f42760938dd3eb629995bd9f
Wirlie <josue.211996@outlook.com>
2016-05-20 00:53:20 -0500

    Make more configurable the generation of ores before to the world generation.

--- AllBanks2/src/main/java/me/wirlie/allbanks/land/generator/FlatOrePopulator.java
+++ AllBanks2/src/main/java/me/wirlie/allbanks/land/generator/FlatOrePopulator.java
@@ -31,7 +31,26 @@ import org.bukkit.generator.BlockPopulator;
  *
  */
 public class FlatOrePopulator extends BlockPopulator {
-	static int[][] relativeBlockPos = new int[15][3];
+	
+	/**
+	 * [00] | x + 0 | y + 0 | z + 1 | <br>
+	 * [01] | x + 1 | y + 0 | z + 1 | <br>
+	 * [02] | x + 1 | y + 0 | z + 0 | <br>
+	 * [03] | x + 0 | y + 1 | z + 0 | <br>
+	 * [04] | x + 0 | y + 1 | z + 1 | <br>
+	 * [05] | x + 1 | y + 1 | z + 1 | <br>
+	 * [06] | x + 1 | y + 1 | z + 0 | <br>
+	 * [07] | x + 0 | y + 0 | z + 2 | <br>
+	 * [08] | x + 2 | y + 0 | z + 2 | <br>
+	 * [09] | x + 2 | y + 0 | z + 0 | <br>
+	 * [10] | x + 0 | y + 2 | z + 0 | <br>
+	 * [11] | x + 0 | y + 2 | z + 2 | <br>
+	 * [12] | x + 2 | y + 2 | z + 2 | <br>
+	 * [13] | x + 2 | y + 2 | z + 0 | <br>
+	 * [14] | x + 0 | y + 0 | z + 1 | <br>
+	 */
+	
+	private static int[][] relativeBlockPos = new int[15][3];
 	
     static{
     	//	x + 0	y + 0	z + 1
@@ -95,36 +114,141 @@ public class FlatOrePopulator extends BlockPopulator {
     	relativeBlockPos[14][1] = 0;
     	relativeBlockPos[14][2] = 3;
     }
+
+    private WorldGenerationCfg worldCfg;
     
-    static final int[] ORES_MAX_Y = {
-    	128, //0 Coal	128
-        64, //1 Iron	64
-        23, //2 Lapis
-        40, //3 Gold	60
-        15, //4 Redstone
-        15, //5 Diamond
-        30, //6 Emerald
+    /**
+     * [0] Coal - y128 <br>
+     * [1] Iron - y64 <br>
+     * [2] Lapis - y23 <br>
+     * [3] Gold - y60 <br>
+     * [4] Redstone - y15 <br>
+     * [5] Diamond - y15 <br>
+     * [6] Emerald - y30 <br>
+     */
+    private int[] configOresMaxY = new int[7];
+    
+    /**
+     * [0] Coal<br>
+     * [1] Iron<br>
+     * [2] Lapis<br>
+     * [3] Gold<br>
+     * [4] Redstone<br>
+     * [5] Diamond<br>
+     * [6] Emerald<br>
+     */
+    
+    private int[][] configOresRand = new int[7][2];
+    
+    /**
+     * [0] Coal<br>
+     * [1] Iron<br>
+     * [2] Lapis<br>
+     * [3] Gold<br>
+     * [4] Redstone<br>
+     * [5] Diamond<br>
+     * [6] Emerald<br>
+     */
+    private boolean[] configOresGenerate = new boolean[7];
+    
+    /**
+     * [0] Coal<br>
+     * [1] Iron<br>
+     * [2] Lapis<br>
+     * [3] Gold<br>
+     * [4] Redstone<br>
+     * [5] Diamond<br>
+     * [6] Emerald<br>
+     */
+    private int[] oresAmount = new int[7];
+    
+    /**
+     * [0] Coal<br>
+     * [1] Iron<br>
+     * [2] Lapis<br>
+     * [3] Gold<br>
+     * [4] Redstone<br>
+     * [5] Diamond<br>
+     * [6] Emerald<br>
+     */
+    private Material[] oresMaterial = new Material[]{
+    		Material.COAL_ORE, 		//[0]
+    		Material.IRON_ORE, 		//[1]
+    		Material.LAPIS_ORE, 	//[2]
+    		Material.GOLD_ORE,		//[3]
+    		Material.REDSTONE_ORE,	//[4]
+    		Material.DIAMOND_ORE,	//[5]
+    		Material.EMERALD_ORE	//[6]
     };
     
-    WorldGenerationCfg worldCfg;
-
     /**
 	 * @param worldCfg
 	 */
 	public FlatOrePopulator(WorldGenerationCfg worldCfg) {
 		this.worldCfg = worldCfg;
+		
+		configOresMaxY[0] = worldCfg.coal_ore_max_y;
+		configOresMaxY[1] = worldCfg.iron_ore_max_y;
+		configOresMaxY[2] = worldCfg.lapis_ore_max_y;
+		configOresMaxY[3] = worldCfg.gold_ore_max_y;
+		configOresMaxY[4] = worldCfg.redstone_ore_max_y;
+		configOresMaxY[5] = worldCfg.diamond_ore_max_y;
+		configOresMaxY[6] = worldCfg.emerald_ore_max_y;
+		
+		configOresRand[0][0] = worldCfg.coal_rand_min;
+		configOresRand[0][1] = worldCfg.coal_rand_max;
+		configOresRand[1][0] = worldCfg.iron_rand_min;
+		configOresRand[1][1] = worldCfg.iron_rand_max;
+		configOresRand[2][0] = worldCfg.lapis_rand_min;
+		configOresRand[2][1] = worldCfg.lapis_rand_max;
+		configOresRand[3][0] = worldCfg.gold_rand_min;
+		configOresRand[3][1] = worldCfg.gold_rand_max;
+		configOresRand[4][0] = worldCfg.redstone_rand_min;
+		configOresRand[4][1] = worldCfg.redstone_rand_max;
+		configOresRand[5][0] = worldCfg.diamond_rand_min;
+		configOresRand[5][1] = worldCfg.diamond_rand_max;
+		configOresRand[6][0] = worldCfg.emerald_rand_min;
+		configOresRand[6][1] = worldCfg.emerald_rand_max;
+		
+		configOresGenerate[0] = worldCfg.coal_ore;
+		configOresGenerate[1] = worldCfg.iron_ore;
+		configOresGenerate[2] = worldCfg.lapis_ore;
+		configOresGenerate[3] = worldCfg.gold_ore;
+		configOresGenerate[4] = worldCfg.redstone_ore;
+		configOresGenerate[5] = worldCfg.diamond_ore;
+		configOresGenerate[6] = worldCfg.emerald_ore;
 	}
 
 	@Override
     public void populate(World world, Random rand, Chunk chunk) {
-    	
-    	int coal_rand = (rand.nextInt(10 - 6) + 6);
-    	int iron_rand = (rand.nextInt(8 - 5) + 5);
-    	int lapis_rand = (rand.nextInt(4));
-    	int gold_rand = (rand.nextInt(3 - 1) + 1);
-    	int redstone_rand = (rand.nextInt(4 - 2) + 2);
-    	int diamond_rand = (rand.nextInt(2));
-    	int emerald_rand = (rand.nextInt(2));
+    	//Coal
+		if(configOresRand[0][0] < 0) configOresRand[0][0] = 0;
+		if(configOresRand[0][1] < configOresRand[0][0]) configOresRand[0][1] = configOresRand[0][0];
+		oresAmount[0] = (rand.nextInt(configOresRand[0][1] - configOresRand[0][0]) + configOresRand[0][0]);
+    	//Iron
+    	if(configOresRand[1][0] < 0) configOresRand[1][0] = 0;
+		if(configOresRand[1][1] < configOresRand[1][0]) configOresRand[1][1] = configOresRand[1][0];
+		oresAmount[1] = (rand.nextInt(configOresRand[1][1] - configOresRand[1][0]) + configOresRand[1][0]);
+    	//Lapis
+    	if(configOresRand[2][0] < 0) configOresRand[2][0] = 0;
+		if(configOresRand[2][1] < configOresRand[2][0]) configOresRand[2][1] = configOresRand[2][0];
+		oresAmount[2] = (rand.nextInt(configOresRand[2][1] - configOresRand[2][0]) + configOresRand[2][0]);
+    	//Gold
+    	if(configOresRand[3][0] < 0) configOresRand[3][0] = 0;
+		if(configOresRand[3][1] < configOresRand[3][0]) configOresRand[3][1] = configOresRand[3][0];
+		oresAmount[3] = (rand.nextInt(configOresRand[3][1] - configOresRand[3][0]) + configOresRand[3][0]);
+    	//Redstone
+    	if(configOresRand[4][0] < 0) configOresRand[4][0] = 0;
+		if(configOresRand[4][1] < configOresRand[4][0]) configOresRand[4][1] = configOresRand[4][0];
+		oresAmount[4] = (rand.nextInt(configOresRand[4][1] - configOresRand[4][0]) + configOresRand[4][0]);
+    	//Diamond
+    	if(configOresRand[5][0] < 0) configOresRand[5][0] = 0;
+		if(configOresRand[5][1] < configOresRand[5][0]) configOresRand[5][1] = configOresRand[5][0];
+		oresAmount[5] = (rand.nextInt(configOresRand[5][1] - configOresRand[5][0]) + configOresRand[5][0]);
+    	//Emerald
+    	if(configOresRand[6][0] < 0) configOresRand[6][0] = 0;
+		if(configOresRand[6][1] < configOresRand[6][0]) configOresRand[6][1] = configOresRand[6][0];
+		oresAmount[6] = (rand.nextInt(configOresRand[6][1] - configOresRand[6][0]) + configOresRand[6][0]);
     	
     	boolean shouldContinue = true;
     	int i = 0;
@@ -132,32 +256,32 @@ public class FlatOrePopulator extends BlockPopulator {
         while(shouldContinue) {
         	shouldContinue = false;
         	
-			if(i < coal_rand && worldCfg.coal_ore){ 
-				populateRandomOre(chunk, rand, Material.COAL_ORE);
+			if(i < oresAmount[0] && configOresGenerate[0]){ 
+				populateRandomOre(chunk, rand, oresMaterial[0]);
 	        	shouldContinue = true;
 			}
-            if(i < iron_rand && worldCfg.iron_ore){
-            	populateRandomOre(chunk, rand, Material.IRON_ORE);
+            if(i < oresAmount[1] && configOresGenerate[1]){
+            	populateRandomOre(chunk, rand, oresMaterial[1]);
 	        	shouldContinue = true;
             }
-            if(i < lapis_rand && worldCfg.lapis_ore){
-            	populateRandomOre(chunk, rand, Material.LAPIS_ORE);
+            if(i < oresAmount[2] && configOresGenerate[2]){
+            	populateRandomOre(chunk, rand, oresMaterial[2]);
 	        	shouldContinue = true;
             }
-            if(i < gold_rand && worldCfg.gold_ore){
-            	populateRandomOre(chunk, rand, Material.GOLD_ORE);
+            if(i < oresAmount[3] && configOresGenerate[3]){
+            	populateRandomOre(chunk, rand, oresMaterial[3]);
 	        	shouldContinue = true;
             }
-            if(i < redstone_rand && worldCfg.redstone_ore){
-            	populateRandomOre(chunk, rand, Material.REDSTONE_ORE);
+            if(i < oresAmount[4] && configOresGenerate[4]){
+            	populateRandomOre(chunk, rand, oresMaterial[4]);
 	        	shouldContinue = true;
             }
-			if(i < diamond_rand && worldCfg.diamond_ore){
-				populateRandomOre(chunk, rand, Material.DIAMOND_ORE);
+			if(i < oresAmount[5] && configOresGenerate[5]){
+				populateRandomOre(chunk, rand, oresMaterial[5]);
 	        	shouldContinue = true;
 			}
-			if(i < emerald_rand && worldCfg.emerald_ore){
-				populateRandomOre(chunk, rand, Material.EMERALD_ORE);
+			if(i < oresAmount[6] && configOresGenerate[6]){
+				populateRandomOre(chunk, rand, oresMaterial[6]);
 	        	shouldContinue = true;
 			}
 			
@@ -165,38 +289,38 @@ public class FlatOrePopulator extends BlockPopulator {
         }
     }
 
-    public void populateRandomOre(Chunk chunk, Random rand, Material material) {
+    private void populateRandomOre(Chunk chunk, Random rand, Material material) {
     	
     	int maxY = 0, min_amount = 3, max_amount = 3;
     	
     	if(material == Material.COAL_ORE){
-    		maxY = ORES_MAX_Y[0];
+    		maxY = configOresMaxY[0];
     		max_amount = 15;
     		min_amount = 5;
     	}
     	if(material == Material.IRON_ORE){
-    		maxY = ORES_MAX_Y[1];
+    		maxY = configOresMaxY[1];
     		max_amount = 12;
     		min_amount = 5;
     	}
     	if(material == Material.LAPIS_ORE){
-    		maxY = ORES_MAX_Y[2];
+    		maxY = configOresMaxY[2];
     		max_amount = 10;
     	}
     	if(material == Material.GOLD_ORE){
-    		maxY = ORES_MAX_Y[3];
+    		maxY = configOresMaxY[3];
     		max_amount = 8;
     	}
     	if(material == Material.REDSTONE_ORE){
-    		maxY = ORES_MAX_Y[4];
+    		maxY = configOresMaxY[4];
     		max_amount = 8;
     	}
     	if(material == Material.DIAMOND_ORE){
-    		maxY = ORES_MAX_Y[5];
+    		maxY = configOresMaxY[5];
     		max_amount = 12;
     	}
     	if(material == Material.EMERALD_ORE){
-    		maxY = ORES_MAX_Y[6];
+    		maxY = configOresMaxY[6];
     		max_amount = 2; 
     		min_amount = 1;
     	}
@@ -209,17 +333,17 @@ public class FlatOrePopulator extends BlockPopulator {
             chunk.getBlock(x, y, z).setType(material);
         }
         
-        this.setOre(material, chunk, x, y, z);
-
+        this.setBlockChunk(material, chunk, x, y, z);
+        
         int ore_amount = rand.nextInt(max_amount - min_amount) + min_amount;
         
         for(int i = 0; i < ore_amount; i++) {
-                this.setOre(material, chunk, x + FlatOrePopulator.relativeBlockPos[i][0], y + FlatOrePopulator.relativeBlockPos[i][1], z + FlatOrePopulator.relativeBlockPos[i][2]);
+                this.setBlockChunk(material, chunk, x + FlatOrePopulator.relativeBlockPos[i][0], y + FlatOrePopulator.relativeBlockPos[i][1], z + FlatOrePopulator.relativeBlockPos[i][2]);
         }
     }
 
-    public void setOre(Material ore, Chunk chunk, int x, int y, int z) {
+    private void setBlockChunk(Material ore, Chunk chunk, int x, int y, int z) {
         Block block = chunk.getBlock(x, y - 1, z);
-        if (block.getType() == Material.STONE) block.setType(ore);
+        if (block.getY() < (worldCfg.world_height - 2) && !block.getType().equals(Material.BEDROCK)) block.setType(ore);
     }
 }
--- AllBanks2/src/main/java/me/wirlie/allbanks/land/generator/WorldGenerationCfg.java
+++ AllBanks2/src/main/java/me/wirlie/allbanks/land/generator/WorldGenerationCfg.java
@@ -33,55 +33,113 @@ import me.wirlie.allbanks.utils.FileDirectory;
  *
  */
 public class WorldGenerationCfg {
-	public static int world_height_def = 60;
+	private static int world_height_def = 60;
 	public int world_height = world_height_def;
 	
-	public static int plot_size_def = 50;
+	private static int plot_size_def = 50;
 	public int plot_size = plot_size_def;
 	
-	public static int road_size_def = 10;
+	private static int road_size_def = 10;
 	public int road_size = road_size_def;
 	
-	public static Material road_material_def = Material.WOOD;
+	private static Material road_material_def = Material.WOOD;
 	public Material road_material = road_material_def;
 	
-	public static boolean coal_ore_def = true;
+	private static boolean coal_ore_def = true;
 	public boolean coal_ore = coal_ore_def;
 	
-	public static boolean iron_ore_def = true;
+	private static boolean iron_ore_def = true;
 	public boolean iron_ore = iron_ore_def;
 	
-	public static boolean emerald_ore_def = true;
+	private static boolean emerald_ore_def = true;
 	public boolean emerald_ore = emerald_ore_def;
 	
-	public static boolean diamond_ore_def = true;
+	private static boolean diamond_ore_def = true;
 	public boolean diamond_ore = diamond_ore_def;
 	
-	public static boolean redstone_ore_def = true;
+	private static boolean redstone_ore_def = true;
 	public boolean redstone_ore = redstone_ore_def;
 	
-	public static boolean gold_ore_def = true;
+	private static boolean gold_ore_def = true;
 	public boolean gold_ore = gold_ore_def;
 	
-	public static boolean lapis_ore_def = true;
+	private static boolean lapis_ore_def = true;
 	public boolean lapis_ore = lapis_ore_def;
 	
-	public static boolean generate_grass_def = true;
+	private static boolean generate_grass_def = true;
 	public boolean generate_grass = generate_grass_def;
 	
-	public static boolean generate_tree_def = true;
+	private static boolean generate_tree_def = true;
 	public boolean generate_tree = generate_tree_def;
 	
-	public static TreeType tree_type_1_def = TreeType.TREE;
+	private static TreeType tree_type_1_def = TreeType.TREE;
 	public TreeType tree_type_1 = tree_type_1_def;
 	
-	public static TreeType tree_type_2_def = TreeType.BIRCH;
+	private static TreeType tree_type_2_def = TreeType.BIRCH;
 	public TreeType tree_type_2 = tree_type_2_def;
 	
-	public static Biome default_biome_def = Biome.PLAINS;
+	private static Biome default_biome_def = Biome.PLAINS;
 	public Biome default_biome = default_biome_def;
 	
 	String world_id = "";
+	
+	//Descriptores de altura máxima para la generacion de un mineral
+	private static int coal_ore_max_y_def = 128;
+	public int coal_ore_max_y = coal_ore_max_y_def;
+
+	private static int iron_ore_max_y_def = 64;
+	public int iron_ore_max_y = iron_ore_max_y_def;
+	
+	private static int lapis_ore_max_y_def = 23;
+	public int lapis_ore_max_y = lapis_ore_max_y_def;
+	
+	private static int gold_ore_max_y_def = 60;
+	public int gold_ore_max_y = gold_ore_max_y_def;
+	
+	private static int redstone_ore_max_y_def = 15;
+	public int redstone_ore_max_y = redstone_ore_max_y_def;
+	
+	private static int diamond_ore_max_y_def = 15;
+	public int diamond_ore_max_y = diamond_ore_max_y_def;
+	
+	private static int emerald_ore_max_y_def = 30;
+	public int emerald_ore_max_y = emerald_ore_max_y_def;
+	
+	//Descriptores de minimo y máximo de un mineral
+	private static int coal_rand_min_def = 6;
+	private static int coal_rand_max_def = 10;
+	public int coal_rand_min = coal_rand_min_def;
+	public int coal_rand_max = coal_rand_max_def;
+	
+	private static int iron_rand_min_def = 5;
+	private static int iron_rand_max_def = 8;
+	public int iron_rand_min = iron_rand_min_def;
+	public int iron_rand_max = iron_rand_max_def;
+
+	private static int lapis_rand_min_def = 0;
+	private static int lapis_rand_max_def = 4;
+	public int lapis_rand_min = lapis_rand_min_def;
+	public int lapis_rand_max = lapis_rand_max_def;
+
+	private static int gold_rand_min_def = 1;
+	private static int gold_rand_max_def = 3;
+	public int gold_rand_min = gold_rand_min_def;
+	public int gold_rand_max = gold_rand_max_def;
+	
+	private static int redstone_rand_min_def = 2;
+	private static int redstone_rand_max_def = 4;
+	public int redstone_rand_min = redstone_rand_min_def;
+	public int redstone_rand_max = redstone_rand_max_def;
+	
+	private static int diamond_rand_min_def = 0;
+	private static int diamond_rand_max_def = 2;
+	public int diamond_rand_min = diamond_rand_min_def;
+	public int diamond_rand_max = diamond_rand_max_def;
+	
+	private static int emerald_rand_min_def = 0;
+	private static int emerald_rand_max_def = 2;
+	public int emerald_rand_min = emerald_rand_min_def;
+	public int emerald_rand_max = emerald_rand_max_def;
 	
 	public static void removeTemporalConfiguration(String worldID){
 		File worldFileCfg = new File(FileDirectory.WORLDS_DATA_FOLDER + File.separator + "world-" + worldID + "-gen-config.yml");
@@ -103,21 +161,48 @@ public class WorldGenerationCfg {
 		Material road_material_try = Material.getMaterial(cfg.getString("road-material", "WOOD"));
 		if(road_material_try  != null) road_material = road_material_try;
 		
-		coal_ore = cfg.getBoolean("generate-ores.coal", coal_ore);
-		iron_ore = cfg.getBoolean("generate-ores.iron", iron_ore);
-		emerald_ore = cfg.getBoolean("generate-ores.emerald", emerald_ore);
-		diamond_ore = cfg.getBoolean("generate-ores.diamond", diamond_ore);
-		redstone_ore = cfg.getBoolean("generate-ores.redstone", redstone_ore);
-		gold_ore = cfg.getBoolean("generate-ores.gold", gold_ore);
-		lapis_ore = cfg.getBoolean("generate-ores.lapis", lapis_ore);
+		//Generar o no generar minerales.
+		coal_ore = cfg.getBoolean("generate-ores.coal.generate", coal_ore);
+		iron_ore = cfg.getBoolean("generate-ores.iron.generate", iron_ore);
+		emerald_ore = cfg.getBoolean("generate-ores.emerald.generate", emerald_ore);
+		diamond_ore = cfg.getBoolean("generate-ores.diamond.generate", diamond_ore);
+		redstone_ore = cfg.getBoolean("generate-ores.redstone.generate", redstone_ore);
+		gold_ore = cfg.getBoolean("generate-ores.gold.generate", gold_ore);
+		lapis_ore = cfg.getBoolean("generate-ores.lapis.generate", lapis_ore);
 		
+		//altura (y) de los minerales
+		coal_ore_max_y = cfg.getInt("generate-ores.coal.max-y", coal_ore_max_y);
+		iron_ore_max_y = cfg.getInt("generate-ores.iron.max-y", iron_ore_max_y);
+		emerald_ore_max_y = cfg.getInt("generate-ores.emerald.max-y", emerald_ore_max_y);
+		diamond_ore_max_y = cfg.getInt("generate-ores.diamond.max-y", diamond_ore_max_y);
+		redstone_ore_max_y = cfg.getInt("generate-ores.redstone.max-y", redstone_ore_max_y);
+		gold_ore_max_y = cfg.getInt("generate-ores.gold.max-y", gold_ore_max_y);
+		lapis_ore_max_y = cfg.getInt("generate-ores.lapis.max-y", lapis_ore_max_y);
+		
+		//aleatoriedad en los minerales
+		coal_rand_min = cfg.getInt("generate-ores.coal.rand_min", coal_rand_min);
+		coal_rand_max = cfg.getInt("generate-ores.coal.rand_max", coal_rand_max);
+		iron_rand_min = cfg.getInt("generate-ores.iron.rand_min", iron_rand_min);
+		iron_rand_max = cfg.getInt("generate-ores.iron.rand_max", iron_rand_max);
+		emerald_rand_min = cfg.getInt("generate-ores.emerald.rand_min", emerald_rand_min);
+		emerald_rand_max = cfg.getInt("generate-ores.emerald.rand_max", emerald_rand_max);
+		diamond_rand_min = cfg.getInt("generate-ores.diamond.rand_min", diamond_rand_min);
+		diamond_rand_max = cfg.getInt("generate-ores.diamond.rand_max", diamond_rand_max);
+		redstone_rand_min = cfg.getInt("generate-ores.redstone.rand_min", redstone_rand_min);
+		redstone_rand_max = cfg.getInt("generate-ores.redstone.rand_max", redstone_rand_max);
+		gold_rand_min = cfg.getInt("generate-ores.gold.rand_min", gold_rand_min);
+		gold_rand_max = cfg.getInt("generate-ores.gold.rand_max", gold_rand_max);
+		lapis_rand_min = cfg.getInt("generate-ores.lapis.rand_min", lapis_rand_min);
+		lapis_rand_max = cfg.getInt("generate-ores.lapis.rand_max", lapis_rand_max);
+		
+		//Arboles y hierba
 		generate_grass = cfg.getBoolean("generate-grass", generate_grass);
-		
 		generate_tree = cfg.getBoolean("generate-tree-type.enable", generate_tree);
-		
+		//Tipo de árboles
+		//TODO Aqui debería ser mas flexible a un listado
 		TreeType tryType1 = TreeType.valueOf(cfg.getString("generate-tree-type.type-1", "TREE"));
 		TreeType tryType2 = TreeType.valueOf(cfg.getString("generate-tree-type.type-2", "BIRCH"));
-	
+		
 		if(tryType1 != null) tree_type_1 = tryType1;
 		if(tryType2 != null) tree_type_2 = tryType2;
 		
@@ -149,13 +234,38 @@ public class WorldGenerationCfg {
 		worldCfg.set("plot_size", plot_size_def);
 		worldCfg.set("road_size", road_size_def);
 		worldCfg.set("road-material", road_material_def.toString());
-		worldCfg.set("generate-ores.coal", coal_ore_def);
-		worldCfg.set("generate-ores.iron", iron_ore_def);
-		worldCfg.set("generate-ores.emerald", emerald_ore_def);
-		worldCfg.set("generate-ores.diamond", diamond_ore_def);
-		worldCfg.set("generate-ores.redstone", redstone_ore_def);
-		worldCfg.set("generate-ores.gold", gold_ore_def);
-		worldCfg.set("generate-ores.lapis", lapis_ore_def);
+		//Generación de minerales
+		worldCfg.set("generate-ores.coal.generate", coal_ore_def);
+		worldCfg.set("generate-ores.iron.generate", iron_ore_def);
+		worldCfg.set("generate-ores.emerald.generate", emerald_ore_def);
+		worldCfg.set("generate-ores.diamond.generate", diamond_ore_def);
+		worldCfg.set("generate-ores.redstone.generate", redstone_ore_def);
+		worldCfg.set("generate-ores.gold.generate", gold_ore_def);
+		worldCfg.set("generate-ores.lapis.generate", lapis_ore_def);
+		//Altura de minerales
+		worldCfg.set("generate-ores.coal.max-y", coal_ore_max_y_def);
+		worldCfg.set("generate-ores.iron.max-y", iron_ore_max_y_def);
+		worldCfg.set("generate-ores.emerald.max-y", emerald_ore_max_y_def);
+		worldCfg.set("generate-ores.diamond.max-y", diamond_ore_max_y_def);
+		worldCfg.set("generate-ores.redstone.max-y", redstone_ore_max_y_def);
+		worldCfg.set("generate-ores.gold.max-y", gold_ore_max_y_def);
+		worldCfg.set("generate-ores.lapis.max-y", lapis_ore_max_y_def);
+		//Minerales aleatorios, parámetros
+		worldCfg.set("generate-ores.coal.rand_min", coal_rand_min_def);
+		worldCfg.set("generate-ores.coal.rand_max", coal_rand_max_def);
+		worldCfg.set("generate-ores.iron.rand_min", iron_rand_min_def);
+		worldCfg.set("generate-ores.iron.rand_max", iron_rand_max_def);
+		worldCfg.set("generate-ores.emerald.rand_min", emerald_rand_min_def);
+		worldCfg.set("generate-ores.emerald.rand_max", emerald_rand_max_def);
+		worldCfg.set("generate-ores.diamond.rand_min", diamond_rand_min_def);
+		worldCfg.set("generate-ores.diamond.rand_max", diamond_rand_max_def);
+		worldCfg.set("generate-ores.redstone.rand_min", redstone_rand_min_def);
+		worldCfg.set("generate-ores.redstone.rand_max", redstone_rand_max_def);
+		worldCfg.set("generate-ores.gold.rand_min", gold_rand_min_def);
+		worldCfg.set("generate-ores.gold.rand_max", gold_rand_max_def);
+		worldCfg.set("generate-ores.lapis.rand_min", lapis_rand_min_def);
+		worldCfg.set("generate-ores.lapis.rand_max", lapis_rand_max_def);
+		//Otros
 		worldCfg.set("generate-grass", generate_grass_def);
 		worldCfg.set("generate-tree-type.enable", generate_tree_def);
 		worldCfg.set("generate-tree-type.type-1", tree_type_1_def.toString());
