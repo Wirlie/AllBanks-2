086b105e0df2c81f2cb2d8cfe1e33204b1df20cd
Wirlie <josue.211996@outlook.com>
2016-05-23 00:43:46 -0500

    Add compatibility for 1_9_R1 and 1_9_R2.

---
+++ AllBanks2/src/main/java/me/wirlie/allbanks/utils/Util_R2.java
@@ -0,0 +1,73 @@
+package me.wirlie.allbanks.utils;
+
+import org.bukkit.ChatColor;
+
+import net.minecraft.server.v1_9_R2.EnumChatFormat;
+import net.minecraft.server.v1_9_R2.NBTTagCompound;
+
+public class Util_R2 {
+	
+	public static String getItemCodeOrGetCustomName(net.minecraft.server.v1_9_R2.ItemStack stack) {
+        NBTTagCompound tag = stack.getTag();
+
+        if ((tag != null) && (tag.hasKeyOfType("display", 10))) {
+            NBTTagCompound nbttagcompound = tag.getCompound("display");
+
+            if (nbttagcompound.hasKeyOfType("Name", 8)) {
+                return EnumChatFormat.ITALIC + nbttagcompound.getString("Name");
+            }
+        }
+
+        return stack.a() + ".name";
+    }
+	
+	public static ChatColor convertEnumChatFormatToChatColor(EnumChatFormat e) {
+		
+		switch(e){
+		case AQUA:
+			return ChatColor.AQUA;
+		case BLACK:
+			return ChatColor.BLACK;
+		case BLUE:
+			return ChatColor.BLUE;
+		case BOLD:
+			return ChatColor.BOLD;
+		case DARK_AQUA:
+			return ChatColor.DARK_AQUA;
+		case DARK_BLUE:
+			return ChatColor.DARK_BLUE;
+		case DARK_GRAY:
+			return ChatColor.DARK_GRAY;
+		case DARK_GREEN:
+			return ChatColor.DARK_GREEN;
+		case DARK_PURPLE:
+			return ChatColor.DARK_PURPLE;
+		case DARK_RED:
+			return ChatColor.DARK_RED;
+		case GOLD:
+			return ChatColor.GOLD;
+		case GRAY:
+			return ChatColor.GRAY;
+		case GREEN:
+			return ChatColor.GREEN;
+		case ITALIC:
+			return ChatColor.ITALIC;
+		case LIGHT_PURPLE:
+			return ChatColor.LIGHT_PURPLE;
+		case RED:
+			return ChatColor.RED;
+		case RESET:
+			return ChatColor.RESET;
+		case STRIKETHROUGH:
+			return ChatColor.STRIKETHROUGH;
+		case UNDERLINE:
+			return ChatColor.UNDERLINE;
+		case WHITE:
+			return ChatColor.WHITE;
+		case YELLOW:
+			return ChatColor.YELLOW;
+		default:
+			return ChatColor.WHITE;
+		}
+	}
+}
