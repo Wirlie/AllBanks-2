0bb8fdc7cdd769fc9e1e4ad0b46f0fcb574948d1
Wirlie <josue.211996@outlook.com>
2016-06-15 21:30:58 -0500

    test

--- AllBanks2/src/main/java/me/wirlie/allbanks/allbanksland/AllBanksPlot.java
+++ AllBanks2/src/main/java/me/wirlie/allbanks/allbanksland/AllBanksPlot.java
@@ -63,7 +63,7 @@ public class AllBanksPlot {
 	private boolean registeredDatabase = false;
 	
 	/**
-	 * Constructor.
+	 * Constructor
 	 * @param abw Mundo de AllBanksLand
 	 * @param plotX X id
 	 * @param plotZ Z id
